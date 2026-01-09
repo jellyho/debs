f069905c3d00d6c1e84354ee5dfeb28367c35690
jellyho <jellyho@naver.com>
2026-01-09 16:33:56 +0900

    download

--- download_ogbench.sh
+++ download_ogbench.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
 cd ~/.ogbench/data
-wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-single-play-v0.npz
-wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-single-play-v0-val.npz
+# wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-single-play-v0.npz
+# wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-single-play-v0-val.npz
 
-wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-double-play-v0.npz
-wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-double-play-v0-val.npz
+# wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-double-play-v0.npz
+# wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-cube-double-play-v0-val.npz
+
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-puzzle-3x3-play-v0.npz
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-puzzle-3x3-play-v0-val.npz
+
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-puzzle-4x4-play-v0.npz
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-puzzle-4x4-play-v0-val.npz
+
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-scene-play-v0.npz
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/visual-scene-play-v0-val.npz
+
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/cube-double-play-v0.npz
+wget -r -np -nH --cut-dirs=2 -A "*.npz" https://rail.eecs.berkeley.edu/datasets/ogbench/cube-double-play-v0-val.npz
